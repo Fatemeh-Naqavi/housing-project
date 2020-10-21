febe6877de0fb4fae65a5724d7a0c3016cc42d28
Fatemeh-Naqavi <f.naqavi91@gmail.com>
2020-10-21 11:15:54 +0200

    primary changes

--- Main.py
+++ Main.py
@@ -2,40 +2,27 @@ import numpy as np
 import pandas as pd
 import scipy.optimize as op
 import Model 
-
 import DataPrep
 
 # %%
-data = DataPrep.get_default_date()
-
-(new_contract_date_series, new_published_ad_date_series) = DataPrep.convert_date_to_days_from_first_published_ad_date(
-    data['contract_date'],
-    data['ad_publicized_date']
-)
-
-one_month_ads = data.loc[data['ad_publicized_date'] < '2005-02-01'].sort_values('ad_publicized_date').copy()
-one_month_contracts = data.loc[data['contract_date'] < '2005-02-01'].sort_values('contract_date').copy()
-
-ad_info_one_month = pd.DataFrame({
-    'contract_date_as_days': new_contract_date_series,
-    'ad_published_date_as_days': new_published_ad_date_series,
-    'living_area': one_month_ads['living_area'],
-    'contract_price': one_month_ads['contract_price']
-}, index=one_month_ads.index)
-
+ad_info_one_month = pd.read_csv('OneMonthData.csv', delimiter = ';')
 contract_info_one_month = DataPrep.one_month_data(ad_info_one_month)
-
 ad_info_one_month['living_area'] = ad_info_one_month['living_area'].apply(lambda x: np.log(x))
-ad_info_one_month['contract_price'] = ad_info_one_month['contract_price'].apply(lambda x: np.log(x))
+ad_info_one_month['contract_price'] = ad_info_one_month['contract_price'].apply(lambda x: (x/1000000))
+contract_info_one_month['contract_price'] = contract_info_one_month['contract_price'].apply(lambda x: np.log(x))
+contract_info_one_month = DataPrep.add_next_larger_price_to_contract_info(contract_info_one_month, ad_info_one_month)
+all_av_ad_df = DataPrep.get_availabe_ads(contract_info_one_month, ad_info_one_month)
 
 # %%
 def log_likelihood(beta):
-    prb = Model.calculate_prob(contract_info_one_month, ad_info_one_month, beta)
+    prb = Model.calculate_prob(contract_info_one_month, ad_info_one_month, all_av_ad_df, beta)
     ll = -np.sum(np.log(prb))
+    print(ll)
     return ll
 
 
-beta = np.array([[1 , 1]])
+beta = np.array([[1] , [2]])
+
 result = op.minimize(log_likelihood, beta, method='CG')
 
 print(result)
