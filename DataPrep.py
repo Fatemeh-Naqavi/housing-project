60c15aef4161d1274b0cb343d9ab41ce865c9652
Fatemeh-Naqavi <f.naqavi91@gmail.com>
2020-10-21 13:09:49 +0200

    speed_improved

--- DataPrep.py
+++ DataPrep.py
@@ -1,46 +1,19 @@
+from statistics import NormalDist
+import scipy.stats as stats
+import numpy as np
 import pandas as pd
+from bisect import bisect_right 
 
-
-df = pd.read_csv("mstat_CSV.csv",low_memory=False)
-
-
-def get_default_date():
-    data = df[['contract_price',
-               'living_area',
-               'contract_date',
-               'real_estate_ad_publicized']].rename(
-        columns={'real_estate_ad_publicized': 'ad_publicized_date'}
-    ).copy()
-    data[['contract_date', 'ad_publicized_date']] = data[['contract_date', 'ad_publicized_date']] \
-        .apply(pd.to_datetime, format='%Y-%m-%d')
-
-    data.dropna(axis=0, how='any', inplace=True)
-    data = data.loc[df['county'] == 'STOCKHOLM']
-    data = data.loc[data['contract_date'] >= data['ad_publicized_date']]
-    data.reset_index(inplace=True, drop=True)
-    return data
-
-
-def convert_date_to_days_from_first_published_ad_date(contract_date_series, published_ad_date_series):
-    first_published_add_date = published_ad_date_series.min()
-    first_published_add_date - published_ad_date_series[0]
-    new_contract_date_series = contract_date_series.apply(
-        lambda x: (x - first_published_add_date).days)
-    new_published_ad_date_series = published_ad_date_series.apply(
-        lambda x: (x - first_published_add_date).days)
-
-    return new_contract_date_series, new_published_ad_date_series
-
-# %%
 
 def one_month_data(data_frame):
-    return data_frame.loc[data_frame['contract_date_as_days'] < 643]
+    return data_frame.loc[data_frame['contract_date_as_days'] < 671].copy() #671 for all first month
 
 # %%
-def get_available_ads(index_row, base_contract_date, ad_info):
+
+def get_available_ads_index(index_row, base_contract_date, ad_info):
     available_ads = []
     for j, item in enumerate(ad_info['contract_date_as_days']):
-        idx = ad_info.index[j]
+        idx = ad_info.index[j]        
         ad_date = ad_info['ad_published_date_as_days'].loc[idx]
         if ad_date > base_contract_date:
             break
@@ -48,5 +21,18 @@ def get_available_ads(index_row, base_contract_date, ad_info):
         day_between_contract_dates = base_contract_date - other_contract_date
         if day_between_contract_dates < 1:
             available_ads.append(ad_info.index[j])
-
     return available_ads
+
+def get_availabe_ads_new_method(contract_info_one_month, ad_info_one_month):
+    av_df = pd.DataFrame(index = contract_info_one_month.index, columns = ['av']).astype(object)
+    
+    for row in contract_info_one_month.itertuples():
+        row_index = getattr(row, 'Index')
+        available_ads_index = get_available_ads_index(
+            row_index,
+            getattr(row, 'contract_date_as_days'), 
+            ad_info_one_month)
+        av_df['av'].loc[row_index] = available_ads_index
+    return av_df
+
+
--- Main.py
+++ Main.py
@@ -5,13 +5,13 @@ import Model
 import DataPrep
 
 # %%
-ad_info_one_month = pd.read_csv('OneMonthData.csv', delimiter = ';')
+ad_info_one_month = pd.read_csv('OneMonthData.csv', delimiter = ';',index_col = 'Index')
 contract_info_one_month = DataPrep.one_month_data(ad_info_one_month)
 ad_info_one_month['living_area'] = ad_info_one_month['living_area'].apply(lambda x: np.log(x))
 ad_info_one_month['contract_price'] = ad_info_one_month['contract_price'].apply(lambda x: (x/1000000))
 contract_info_one_month['contract_price'] = contract_info_one_month['contract_price'].apply(lambda x: np.log(x))
-contract_info_one_month = DataPrep.add_next_larger_price_to_contract_info(contract_info_one_month, ad_info_one_month)
-all_av_ad_df = DataPrep.get_availabe_ads(contract_info_one_month, ad_info_one_month)
+ad_info_one_month = ad_info_one_month.sort_values(by = 'ad_published_date_as_days')
+all_av_ad_df = DataPrep.get_availabe_ads_new_method(contract_info_one_month, ad_info_one_month)
 
 # %%
 def log_likelihood(beta):
